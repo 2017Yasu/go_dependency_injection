a3ff97af12a9fc66c6ac6ada6cd1abc8a5cc2c86
2017Yasu <yasu.nkym.96@gmail.com>
2022-10-02 16:47:51 +0900

    created controller type for http handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"net/http"
 )
 
 func LogOutput(message string) {
@@ -74,6 +75,23 @@ func NewSimpleLogic(l Logger, ds DataStore) SimpleLogic {
 
 type Logic interface {
 	SayHello(userID string) (string, error)
+}
+
+type Controller struct {
+	l     Logger
+	logic Logic
+}
+
+func (c Controller) SayHello(w http.ResponseWriter, r *http.Request) {
+	c.l.Log("In SayHello")
+	userID := r.URL.Query().Get("user_id")
+	message, err := c.logic.SayHello(userID)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte(message))
 }
 
 func main() {
