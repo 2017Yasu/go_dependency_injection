998b15da69c952c86b7e1d3dd77e520ea119300d
2017Yasu <yasu.nkym.96@gmail.com>
2022-10-02 16:15:44 +0900

    make LogOutput function meet Logger interface

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ func LogOutput(message string) {
 	fmt.Println(message)
 }
 
+type LoggerAdapter func(message string)
+
+func (lg LoggerAdapter) Log(message string) {
+	lg(message)
+}
+
 type SimpleDataStore struct {
 	userData map[string]string
 }
